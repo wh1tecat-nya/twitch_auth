53d84fe306fa87db869281882a16f1d16f5ced05
wh1tecat <wh1tecat.sec@gmail.com>
2023-02-12 14:32:09 +0900

    add: pm2用のconfigファイルを追加

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "twitch_auto_shoutout",
+      script: "./server.js",
+      time: true,
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+    },
+  ],
+};
